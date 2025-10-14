bbc8235bcb3753b34ee2d93aede752b8e740e356
cz9dev <cz9dev@gmail.com>
2025-10-14 12:59:31 -0400

    Agregando funcionalidad para mostrar y testear configuracion ldap

--- config/middleware.js
+++ config/middleware.js
@@ -30,6 +30,7 @@ const configureMiddleware = (app) => {
   app.use((req, res, next) => {
     res.locals.success_msg = req.flash("success_msg");
     res.locals.error_msg = req.flash("error_msg");
+    res.locals.warning_msg = req.flash("warning_msg");
     res.locals.user = req.user || null;
     next();
   });
